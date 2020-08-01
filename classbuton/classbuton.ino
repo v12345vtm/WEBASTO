8eb26559f4a26d8984a5c1b3639151b50a7ad07e
v12345vtm <plekke@yahoo.com>
2020-08-01 20:09:50 +0200

    Add files via upload

--- classbuton/classbuton.ino
+++ classbuton/classbuton.ino
@@ -5,6 +5,7 @@ class Button {
     byte pin;
     byte flag;
     byte lastflag;
+    byte flank;  //status : 2= hoog nivo  1=rijzende flank  0=dalende flank
 
   public:
     Button(byte pin) {
@@ -21,22 +22,24 @@ class Button {
     void update() {
       // read the pushbutton input pin:
       flag = digitalRead(pin);
-
+      flank = 2 ;
       // compare the SW1flag to its previous state
       if (flag != lastflag) {
         // if the state has changed, increment the counter
         if (flag == HIGH) {
           // if the current state is HIGH then the button went from off to on:
-          Serial.print("pin  ");
-          Serial.print(pin );
-          Serial.println("uit");
+          //  Serial.print("pin  ");
+          //  Serial.print(pin );
+          // Serial.println("in");
+          flank = 1;
           // Serial.print("number of button pushes: ");
           // Serial.println(buttonPushCounter);
         } else {
           // if the current state is LOW then the button went from on to off:
-          Serial.print("pin  ");
-          Serial.print(pin );
-          Serial.println("in");
+          //   Serial.print("pin  ");
+          // Serial.print(pin );
+          // Serial.println("uit");
+          flank = 0;
         }
         // Delay a little bit to avoid bouncing
         delay(50);
@@ -54,6 +57,26 @@ class Button {
     bool isPressed() {
       return (getState() == HIGH);
     }
+
+
+    bool IsErFlank() {
+      update();
+      if (flank == 1) //0 is dalende flank  en 1 is stijgende flank
+      {
+        // Serial.println("up+1");
+        flank = 2 ;//flank is weg zet terug op status2
+        return true ;
+      }
+      else
+      {
+        return false;
+      }
+
+    }
+
+
+
+
 }; // don't forget the semicolon at the end of the class
 
 
@@ -72,9 +95,35 @@ void setup() {
 }
 void loop() {
 
-  button1.getState();
-  button2.getState();
-  button3.getState();
-  button4.getState();
+  controleerInputs();
 
+
+
+  //  button4.getState();
+
+}
+
+
+
+
+
+
+void controleerInputs()
+{
+  if ( button1.IsErFlank())
+  {
+    Serial.println("but1 flank+1");
+  }
+  if ( button2.IsErFlank())
+  {
+    Serial.println("but2 flank+1");
+  }
+  if ( button3.IsErFlank())
+  {
+    Serial.println("but3 flank+1");
+  }
+  if ( button4.IsErFlank())
+  {
+    Serial.println("but4 flank+1");
+  }
 }
